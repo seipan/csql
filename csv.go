67508fca98704ab62a42c9f6c45473d647e202c1
yamasaki <yamasakipann0218@gmail.com>
2023-07-09 23:30:30 +0900

    feat : csv read

--- csv.go
+++ csv.go
@@ -1,1 +1,41 @@
 package csql
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type CsvFile struct {
+	path        string
+	content     [][]string
+	tableSchema map[string]string
+}
+
+func (c *CsvFile) ReadCsvFile() ([][]string, error) {
+	file, err := os.Open(c.path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	r := csv.NewReader(file)
+	rows, err := r.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read csv file: %w", err)
+	}
+	return rows, nil
+}
+
+func (c *CsvFile) SetContent() error {
+	str, err := c.ReadCsvFile()
+	if err != nil {
+		return err
+	}
+	c.content = str
+	return nil
+}
+
+func (c *CsvFile) GetTableSchema() {
+
+}
